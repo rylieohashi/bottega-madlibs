5b75298e933da2fd5b05da2141c4908fb2fc3cb8
rylieohashi <xxryliex@gmail.com>
2021-03-01 22:58:41 -0500

    2 way binding with inputs on card

--- src/components/card.js
+++ src/components/card.js
@@ -11,21 +11,25 @@ class Card extends Component {
             color: 'BLUE',
             pluralNoun: ''
         }
+// we're taking our function and binding it to the instance in our class
+        this.handleInputChange = this.handleInputChange.bind(this);
     }
 
-    handleInputChange() {
-        this.setState({color: 'red' })
+//this is two-way binding and will show what you type in the input. changing the value of state.    
+    handleInputChange(event) {
+        this.setState({color: event.target.value })
     }
 
 // what you change up there in set.State changes down here
 // you want to use state vs setState({}) so you don't get stuck in an infinite loop
 // you can't set state inside of your constructor
 
+//need to turn event to arrow function
     render() {
         return (
             <div className="card">
                 <h1>{this.state.color}</h1>
-                <input/>
+                <input onChange={(event) => this.handleInputChange(event)}/>
             </div>
         )
 
