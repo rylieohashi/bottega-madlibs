bfec8e84426037f42988fdf692ec7da7d0d61460
rylieohashi <xxryliex@gmail.com>
2021-03-03 12:52:32 -0500

    modified handleInputChange to handle multiple input cases instead of just color

--- src/components/card.js
+++ src/components/card.js
@@ -8,7 +8,7 @@ class Card extends Component {
         super()
 
         this.state = {
-            color: 'BLUE',
+            color: '',
             pluralNoun: ''
         }
 // we're taking our function and binding it to the instance in our class
@@ -17,27 +17,22 @@ class Card extends Component {
 
 //this is two-way binding and will show what you type in the input. changing the value of state.    
     handleInputChange(event) {
-        this.setState({color: event.target.value })
-    }
+        this.setState({[event.target.name]: event.target.value })
+        console.log(this.state);
+        }
 
 // what you change up there in set.State changes down here
 // you want to use state vs setState({}) so you don't get stuck in an infinite loop
 // you can't set state inside of your constructor
 
 //need to turn event to arrow function
+//we deleted the arrow function because there's a better syntax
     render() {
-        return (
-            <div className="card">
-                <h1>{this.state.color}</h1>
-                <input onChange={(event) => this.handleInputChange(event)}/>
-            </div>
-        )
-
         return (
             <div className="card"> 
                 <h1>{this.state.color}</h1> 
-                { Input('Color') }
-                { Input('Plural Noun')}
+                { Input('Color', this.state.color, this.handleInputChange, 'color') }
+                { Input('Plural Noun', this.state.pluralNoun, this.handleInputChange, 'pluralNoun')}
             </div>
         )
     }
